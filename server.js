d103918a4072d4e04a330e0c817636104339d2bd
racksoup <rack.connor1@gmail.com>
2023-07-26 10:51:22 -0400

    try1

--- server.js
+++ server.js
@@ -41,4 +41,5 @@ app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
 // axios.get(`http://localhost:${PORT}/api/youtubev3`);
 
+console.log(ip.address())
 axios.post(`http://${ip.address()}:${PORT}/api/twitch/auth`)
