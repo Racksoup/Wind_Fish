210b6ac77d8ad37c1db223527d76b202fc007e03
racksoup <rack.connor1@gmail.com>
2023-07-26 12:07:28 -0400

    try12

--- server.js
+++ server.js
@@ -43,5 +43,6 @@ app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
 console.log(process.env.RENDER_EXTERNAL_URL)
 console.log(ip.address())
+console.log(PORT)
 // axios.post(`http://${ip.address()}:${PORT}/api/twitch/auth`)
-axios.post(`http://wind-fish.onrender.com:${PORT}/api/twitch/auth`)
+// axios.post(`http://wind-fish.onrender.com:${PORT}/api/twitch/auth`)
